efc80ebf4efb2a0ada7d9a6528eef6b94d6cffee
Ricardo-wurzmann <ricardow3@al.insper.edu.br>
2024-04-17 20:09:45 +0000

    JOYSTICK 1 FUNCIOANNDO

--- main/main.c
+++ main/main.c
@@ -10,43 +10,44 @@
 // Definições dos pinos para os joysticks
 const uint ADC_PIN_X1 = 26; // GPIO 26, ADC Channel 0
 const uint ADC_PIN_Y1 = 27; // GPIO 27, ADC Channel 1
-const uint ADC_PIN_X2 = 28; // GPIO 28, ADC Channel 2
-const uint ADC_PIN_Y2 = 29; // GPIO 29, ADC Channel 3
+// const uint ADC_PIN_X2 = 28; // GPIO 28, ADC Channel 2
+
+const uint BUTTON_PIN = 15;
 
 QueueHandle_t xQueueAdcData;
 QueueHandle_t xQueueCommands;
 
 typedef struct {
+    int dici;  // Dicionario [1 -- joystick, 2 -- AWSD, 3 -- Botoes]
     int axis;  // 0 para X, 1 para Y
-    int val;
-    char command[10];  // Comando, se aplicável
+    int val;  // valor ou botao
 } joystick_data_t;
 
 
 // Tarefas para o joystick 1
 void x1_task(void *params) {
-    joystick_data_t data = {.axis = 0, .command = ""};
+    joystick_data_t data = {.dici = 1, .axis = 0};
     while (1) {
         adc_select_input(0); // ADC Channel 0
-        if ((adc_read() - 2047) / 8 > -30 && (adc_read() - 2047) / 8 < 30) {
+        if ((adc_read() - 2047) / 8 > -40 && (adc_read() - 2047) / 8 < 40) {
             data.val = 0;
         } else {
-            data.val = (adc_read() - 2047) / 8;  // Lê o valor ADC do eixo X
+            data.val = (adc_read() - 2047) / 12;  // Lê o valor ADC do eixo X
         }
         xQueueSend(xQueueAdcData, &data, portMAX_DELAY);
-        vTaskDelay(pdMS_TO_TICKS(10000));
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
 
 void y1_task(void *params) {
-    joystick_data_t data = {.axis = 1, .command = ""};
+    joystick_data_t data = {.dici = 1, .axis = 1};
     while (1) {
         adc_select_input(1); // ADC Channel 1
-        if ((adc_read() - 2047) / 8 > -30 && (adc_read() - 2047) / 8 < 30) {
+        if ((adc_read() - 2047) / 8 > -40 && (adc_read() - 2047) / 8 < 40) {
             data.val = 0;
         } else {
-            data.val = (adc_read() - 2047) / 8;  // Lê o valor ADC do eixo X
+            data.val = (adc_read() - 2047) / 12;  // Lê o valor ADC do eixo X
         }
         xQueueSend(xQueueAdcData, &data, portMAX_DELAY);
         vTaskDelay(pdMS_TO_TICKS(100));
@@ -54,58 +55,94 @@ void y1_task(void *params) {
 }
 
 // Tarefas para o joystick 2
-void x2_task(void *params) {
-    joystick_data_t data = {.axis = 0, .command = ""};
-    while (1) {
-        adc_select_input(2); // ADC Channel 2
-        int x_val = (adc_read() - 2047) / 8;
-        if (x_val < -30) {
-            strcpy(data.command, "botao A");
-        } else if (x_val > 30) {
-            strcpy(data.command, "botao D");
-        } else {
-            strcpy(data.command, "");
-        }
-        xQueueSend(xQueueCommands, &data, portMAX_DELAY);
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
+// void x2_task(void *params) {
+//     joystick_data_t data = {.dici = 2, .axis = 0};
+//     while (1) {
+//         adc_select_input(2); // ADC Channel 2
+//         int x_val = (adc_read() - 2047) / 8;
+//         if (x_val < -30) {
+//             data.val = 1; //A
+//         } else if (x_val > 30) {
+//             data.val = 2; //D
+//         } else {
+//             data.val = 0;
+//         }
+//         xQueueSend(xQueueCommands, &data, portMAX_DELAY);
+//         vTaskDelay(pdMS_TO_TICKS(100));
+//     }
+// }
 
-void y2_task(void *params) {
-    joystick_data_t data = {.axis = 1, .command = ""};
-    while (1) {
-        adc_select_input(3); // ADC Channel 3
-        int y_val = (adc_read() - 2047) / 8;
-        if (y_val < -30) {
-            strcpy(data.command, "botao S");
-        } else if (y_val > 30) {
-            strcpy(data.command, "botao W");
-        } else {
-            strcpy(data.command, "");
-        }
-        xQueueSend(xQueueCommands, &data, portMAX_DELAY);
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
+// void y2_task(void *params) {
+//     joystick_data_t data = {.dici = 2, .axis = 0};
+//     while (1) {
+//         adc_select_input(3); // ADC Channel 3
+//         int y_val = (adc_read() - 2047) / 8;
+//         if (y_val < -30) {
+//             data.val = 3; //S
+//         } else if (y_val > 30) {
+//             data.val = 4; //W
+//         } else {
+//             data.val = 0; // Certifique-se de limpar o comando se estiver dentro do limiar
+//         }
+//         xQueueSend(xQueueCommands, &data, portMAX_DELAY);
+//         vTaskDelay(pdMS_TO_TICKS(100));
+//     }
+// }
+
+
+
+// void button_space_task(void *params) {
+//     joystick_data_t data = {.dici = 2, .axis = -1, .val = 3};
+//     bool buttonPressed = false;
+
+//     while (1) {
+//         if (gpio_get(BUTTON_PIN) == 0) {  // Se o botão estiver pressionado (leitura baixa)
+//             if (!buttonPressed) {  // Verifica se o botão foi apenas pressionado
+//                 buttonPressed = true;
+//                 xQueueSend(xQueueAdcData, &data, portMAX_DELAY);
+//             }
+//         } else {
+//             buttonPressed = false;  // Reseta o estado quando o botão é solto
+//         }
+//         vTaskDelay(pdMS_TO_TICKS(50));  // Debounce delay
+//     }
+// }
 
 
 // Função para formatar e enviar dados ou comandos via UART
 void write_package(joystick_data_t data) {
-    if (data.command[0] != '\0') {
-        // Se há um comando, envie o comando
-        //printf("Sending Command: Axis %d, Command %s\n", data.axis, data.command);
-        uart_puts(uart0, "Command: ");
-        uart_puts(uart0, data.command);
-        uart_puts(uart0, "\n");
-    } else {
-        // Caso contrário, envie os dados ADC
-        printf("Sending ADC Data (Não command): Axis %d, Value %d\n", data.axis, data.val);
+
+    //printf("%d /n",data.dici);
+
+    if (data.dici == 1){
         int msb = data.val >> 8;
         int lsb = data.val & 0xFF;
-        uart_putc_raw(uart0, data.axis);  // Converte o índice do eixo para caracter
+        //printf("DENTRO DICI_1 /n");
+        uart_putc_raw(uart0, 1);  
+        uart_putc_raw(uart0, data.axis);  
         uart_putc_raw(uart0, lsb);
         uart_putc_raw(uart0, msb);
-        uart_putc_raw(uart0, '\n');
+        uart_putc_raw(uart0, -1);
+    }else if(data.dici == 2){
+        uart_putc_raw(uart0, 2);  
+        if(data.val == 1){
+            uart_putc_raw(uart0, 1);  
+        }else if(data.val == 2){
+            uart_putc_raw(uart0, 2); 
+        }else if(data.val == 3){
+            uart_putc_raw(uart0, 3); 
+        }else if(data.val == 4){
+            uart_putc_raw(uart0, 4); 
+        }
+        uart_putc_raw(uart0, 0);  
+        uart_putc_raw(uart0, 0);  
+        uart_putc_raw(uart0, -1);
+    }else if(data.dici == 3){
+        uart_putc_raw(uart0, 3);  
+        uart_putc_raw(uart0, 0);  
+        uart_putc_raw(uart0, 0);  
+        uart_putc_raw(uart0, 0);  
+        uart_putc_raw(uart0, -1);
     }
 }
 
@@ -114,15 +151,14 @@ void uart_task(void *params) {
     joystick_data_t data;
     while (1) {
         if (xQueueReceive(xQueueAdcData, &data, portMAX_DELAY)) {
-            printf("Received ADC Data (Não command): Axis %d, Value %d\n", data.axis, data.val);
             write_package(data);
         }
-        if (xQueueReceive(xQueueCommands, &data, portMAX_DELAY) && data.command[0] != '\0') {
-            printf("Received Command: Axis %d, Command %s\n", data.axis, data.command);
-            write_package(data);
-        }
+        // if (xQueueReceive(xQueueCommands, &data, portMAX_DELAY)) {
+        //     write_package(data);
+        // }
     }
 }
+
 
 
 int main() {
@@ -133,8 +169,11 @@ int main() {
     // Configura os pinos GPIO para o ADC
     adc_gpio_init(ADC_PIN_X1);
     adc_gpio_init(ADC_PIN_Y1);
-    adc_gpio_init(ADC_PIN_X2);
-    adc_gpio_init(ADC_PIN_Y2);
+    // adc_gpio_init(ADC_PIN_X2);
+    // gpio_init(BUTTON_PIN);
+    // gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    // gpio_pull_up(BUTTON_PIN);
+
 
     xQueueAdcData = xQueueCreate(10, sizeof(joystick_data_t));
     xQueueCommands = xQueueCreate(10, sizeof(joystick_data_t));
@@ -142,8 +181,9 @@ int main() {
     // Cria tarefas para cada eixo de cada joystick
     xTaskCreate(x1_task, "X1_Task", 256, NULL, 1, NULL);
     xTaskCreate(y1_task, "Y1_Task", 256, NULL, 1, NULL);
-    xTaskCreate(x2_task, "X2_Task", 256, NULL, 1, NULL);
-    xTaskCreate(y2_task, "Y2_Task", 256, NULL, 1, NULL);
+    // xTaskCreate(x2_task, "X2_Task", 256, NULL, 1, NULL);
+    // xTaskCreate(y2_task, "Y2_Task", 256, NULL, 1, NULL);
+    // xTaskCreate(button_space_task, "button_space_task", 256, NULL, 1, NULL);
     xTaskCreate(uart_task, "UART_Task", 256, NULL, 1, NULL);
 
     vTaskStartScheduler(); // Inicia o escalonador
